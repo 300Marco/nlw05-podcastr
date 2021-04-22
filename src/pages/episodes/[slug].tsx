13d4af223e86f9a914e39ab5525046f310bde234
Marco Santana <marcos.maio96@gmail.com>
2021-04-22 20:32:50 -0300

    NLW lesson 4 complete

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,8 +1,9 @@
 import { format, parseISO } from 'date-fns';
-import { ptBR } from 'date-fns/locale';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import ptBR from 'date-fns/locale/pt-BR'
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
@@ -10,38 +11,38 @@ import { convertDurationToTimeString } from '../../utils/convertDurationToTimeSt
 import styles from './episode.module.scss';
 
 type Episode = {
-  id: string;
-  title: string;
-  thumbnail: string;
-  members: string;
-  description: string;
-  duration: number;
-  durationAsString: string;
-  url: string;
-  publishedAt: string;
+  id: string,
+  title: string,
+  thumbnail: string,
+  members: string,
+  publishedAt: string,
+  duration: number,
+  durationAsString: string,
+  url: string,
+  description: string
 }
 
 type EpisodeProps = {
-  episode: Episode[]
+  episode: Episode;
 }
 
 export default function Episode({ episode }: EpisodeProps) {
   return (
-    <div className={styles.episodes}>
+    <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
         <Link href='/'>
-          <button type="button">
-            <img src="/arrow-left.svg" alt="Voltar"/>
+          <button type="button">  
+            <img src="/arrow-left.svg" alt="Voltar" />
           </button>
         </Link>
         <Image 
-          width={700} 
-          height={160}
-          src={episode.thumbnail}
-          objectFit="cover"
+        width={700}
+        height={160}
+        src={episode.thumbnail}
+        objectFit="cover" 
         />
         <button type="button">
-          <img src="/play.svg" alt="Tocar episódio"/>
+          <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
 
@@ -53,41 +54,57 @@ export default function Episode({ episode }: EpisodeProps) {
       </header>
 
       <div 
-        className={styles.description} 
-        dangerouslySetInnerHTML={{ __html: episode.description }}
-      /> 
-    </div>  
+      className={styles.description}
+      dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
   )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+  
   return {
-    paths: [],
-    fallback: 'blocking'
+    paths,
+    fallback: "blocking",
   }
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params;
 
-  const { data } = await api.get(`episodes/${slug}`)
+  const { data } = await api.get(`/episodes/${slug}`)
 
   const episode = {
-      id: data.id,
-      title: data.title,
-      thumbnail: data.thumbnail,
-      members: data.members,
-      publishedAt: format(parseISO(data.published_at), 'd MMM yy', {locale: ptBR}),
-      duration: Number(data.file.duration),
-      durationAsString: convertDurationToTimeString(Number(data.file.duration)),
-      description: data.description,
-      url: data.file.url,
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
   }
-
+  
   return {
     props: {
       episode,
     },
-    revalidate: 60 * 60 * 24, // 24 hours
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
