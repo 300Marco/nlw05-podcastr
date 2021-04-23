df78ef7c6b1bbc3e14dc672c20c0c6f5bede02f5
Marco Santana <marcos.maio96@gmail.com>
2021-04-23 16:18:34 -0300

    remove components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
-  return (
+  return(
     <div className={styles.wapper}>
       <main>
         <Header />
