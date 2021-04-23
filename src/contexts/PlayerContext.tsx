99964b0941799bb21450e7bc1158a20dfefd24c5
Marco Santana <marcos.maio96@gmail.com>
2021-04-23 16:35:29 -0300

    Updated code and adder components for TypeScript

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState, ReactNode } from "react";
 
 type Episode = {
     title: string;
@@ -19,3 +19,41 @@ type PlayerContext = {
 
 export const PlayerContext = createContext({} as PlayerContext);
 
+type PlayerContextProviderProps = {
+    children: ReactNode;
+}
+
+export function PlayerContextProvider( {children}: PlayerContextProviderProps ) {
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    function play(episode: Episode) {
+        setEpisodeList([episode]);
+        setCurrentEpisodeIndex(0);
+        setIsPlaying(true);
+    }
+
+    function togglePlay() {
+        setIsPlaying(!isPlaying);
+    }
+
+    function setPlayingState(state: boolean) {
+        setIsPlaying(state);
+    }
+
+    return (
+        <PlayerContext.Provider
+            value={{ 
+                episodeList, 
+                currentEpisodeIndex, 
+                play, 
+                isPlaying, 
+                togglePlay, 
+                setPlayingState 
+            }}
+        >
+            {children}
+        </PlayerContext.Provider>
+    )
+}
