4f4d15302c720076d6135f2d400b0a1b2e746a38
Marco Santana <marcos.maio96@gmail.com>
2021-04-23 22:16:37 -0300

    Created current time control in the player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Slider from "rc-slider";
 
 import "rc-slider/assets/index.css";
@@ -7,9 +7,11 @@ import "rc-slider/assets/index.css";
 import { usePlayer } from '../../contexts/PlayerContext';
 
 import styles from './styles.module.scss';
+import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
 
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [progress, setProgress] = useState(0);
 
   const {
     episodeList, 
@@ -24,7 +26,7 @@ export function Player() {
     playNext,
     playPrevious,
     hasNext,
-    hasPrevious
+    hasPrevious,
   } = usePlayer();
 
   useEffect(() => {
@@ -38,6 +40,14 @@ export function Player() {
       audioRef.current.pause();
     }
   }, [isPlaying])
+
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0;
+
+    audioRef.current.addEventListener('timeupdate', () => {
+      setProgress(Math.floor(audioRef.current.currentTime));
+    });
+  }
 
   const episode = episodeList[currentEpisodeIndex]
   
@@ -67,10 +77,12 @@ export function Player() {
 
       <footer className={!episode ? styles.empty : ""}>
         <div className={styles.progress}>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(progress)}</span>
           <div className={styles.slider}>
             { episode ? (
-              <Slider 
+              <Slider
+                max={episode.duration}
+                value={progress}
                 trackStyle={{ backgroundColor: "#04d361" }}
                 railStyle={{ backgroundColor: "#9f75ff" }}
                 handleStyle={{ borderColor: "#04d361", borderWidth: 3 }}
@@ -79,7 +91,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             ) }
           </div>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
         { episode && (
@@ -90,8 +102,9 @@ export function Player() {
             autoPlay
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
+            onLoadedMetadata={setupProgressListener}
           />
-        ) }
+        )}
 
         <div className={styles.buttons}>
           <button 
